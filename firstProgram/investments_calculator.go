27677fd11cef8187b00c2ad1e23f10d371608d4c
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2025-05-19 16:42:56 -0300

    projects

---
+++ firstProgram/investments_calculator.go
@@ -0,0 +1,15 @@
+﻿package main
+
+import "math"
+
+func calculateInvestmentReturn(principal float64, rate float64, years float64) float64 {
+	var futureValue = principal * math.Pow(1+rate/100, years)
+
+	return futureValue
+}
+
+func calculateInflationAdjustedReturn(futureValue float64, inflationRate float64, years float64) float64 {
+	var inflationAdjusted = futureValue / math.Pow(1+inflationRate/100, years)
+
+	return inflationAdjusted
+}
---
+++ firstProgram/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter the expected annual return rate (in %): ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	finalAmount := calculateInvestmentReturn(investmentAmount, expectedReturnRate, years)
+	futureRealValue := calculateInflationAdjustedReturn(finalAmount, inflationRate, years)
+
+	fmt.Printf("The final amount after %f years is: $%.2f\n", years, finalAmount)
+	fmt.Printf("The inflation-adjusted value of the investment is: $%.2f\n", futureRealValue)
+}
